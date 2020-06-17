884ae22a3510344032215e988bb86e7ce0a262b4
Andres Ruiz <andrsruiz@gmail.com>
2020-06-17 11:33:29 -0500

    Add Style Loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,16 @@ module.exports = {
 			'vue$': 'vue/dist/vue.esm.js',
 		},
 	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+		],
+	},
 	mode: 'development',
 };
