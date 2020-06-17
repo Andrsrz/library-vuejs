bbf291a35cf5b24439131f719f8e0c033cf40711
Andres Ruiz <andrsruiz@gmail.com>
2020-06-17 17:18:51 -0500

    Add Computed Properties

--- src/index.js
+++ src/index.js
@@ -42,10 +42,46 @@ const Index = (() => {
 		});
 	}
 
+	const computedProperties = () => {
+		new Vue({
+			el: '#root3',
+			data: {
+				message: 'Hello, Vue!',
+				tasks: [
+					{ description: "Play Don't Starve", completed: false },
+					{ description: "Learn Vue", completed: false },
+					{ description: "Push to Github", completed: true },
+					{ description: "Conquer the World", completed: false },
+					{ description: "Watch Community", completed: false }
+				]
+			},
+			methods: {
+				changeCompletition(task){
+					if(task.completed)
+						task.completed = false;
+					else
+						task.completed = true;
+				}
+			},
+			computed: {
+				reversedMessage(){
+					return this.message.split('').reverse().join('');
+				},
+				incompletedTasks(){
+					return this.tasks.filter(task => !task.completed);
+				},
+				completedTasks(){
+					return this.tasks.filter(task => task.completed);
+				}
+			}
+		});
+	}
+
 	const render = () => {
 		dataBinding();
 		workingWithLists();
 		styles();
+		computedProperties();
 	}
 
 	return { render };
