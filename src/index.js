f78a0fa8c05ac5b1a5c9605ba76e0ca7d76b3de8
Andres Ruiz <andrsruiz@gmail.com>
2020-06-18 17:18:15 -0500

    Delete Tutorial Parts

--- src/index.js
+++ src/index.js
@@ -36,8 +36,8 @@ const Index = (() => {
 			<div>
 				<slot></slot>
 				<span>
-					<button @click="changeRead"></button>
-					<button @click="deleteBook"></button>
+					<button @click="changeRead">Read?</button>
+					<button @click="deleteBook">Delete</button>
 				</span>
 			</div>
 		`,
@@ -73,139 +73,6 @@ const Index = (() => {
 			data: {
 				books: myLibrary.books
 			}
-		});
-
-		dataBinding();
-		workingWithLists();
-		styles();
-		computedProperties();
-		components();
-	}
-
-	const dataBinding = () => {
-		new Vue({
-			el: '#root0',
-			data: {
-				message: 'Hello, Vue!'
-			}
-		});
-	}
-
-	const workingWithLists = () => {
-		new Vue({
-			el: '#root1',
-			data: {
-				games: ['The Legend of Zelda', 'The Binding of Isaac', 'Minecraft', "Don't Starve", 'Enter the Gungeon'],
-				newGame: '',
-			},
-			methods: {
-				addGame(){
-					this.games.push(this.newGame);
-					this.newGame = '';
-				}
-			}
-		});
-	}
-
-	const styles = () => {
-		new Vue({
-			el: '#root2',
-			data: {
-				className: 'color-red',
-				isLoading: false
-			},
-			methods: {
-				toggleClass(){
-					this.isLoading = true
-				}
-			}
-		});
-	}
-
-	const computedProperties = () => {
-		new Vue({
-			el: '#root3',
-			data: {
-				message: 'Hello, Vue!',
-				tasks: [
-					{ description: "Play Don't Starve", completed: false },
-					{ description: "Learn Vue", completed: false },
-					{ description: "Push to Github", completed: true },
-					{ description: "Conquer the World", completed: false },
-					{ description: "Watch Community", completed: false }
-				]
-			},
-			methods: {
-				changeCompletition(task){
-					if(task.completed)
-						task.completed = false;
-					else
-						task.completed = true;
-				}
-			},
-			computed: {
-				reversedMessage(){
-					return this.message.split('').reverse().join('');
-				},
-				incompletedTasks(){
-					return this.tasks.filter(task => !task.completed);
-				},
-				completedTasks(){
-					return this.tasks.filter(task => task.completed);
-				}
-			}
-		});
-	}
-
-	Vue.component('task', {
-		template: '<li><slot></slot></li>'
-	});
-
-	Vue.component('tasks-list', {
-		template: `
-			<div>
-				<task v-for="task in tasks">{{ task.description }}</task>
-			</div>
-		`,
-		data(){
-			return {
-				tasks: [
-					{ description: "Play Don't Starve", completed: false },
-					{ description: "Learn Vue", completed: false },
-					{ description: "Push to Github", completed: true },
-					{ description: "Conquer the World", completed: false },
-					{ description: "Watch Community", completed: false }
-				]
-			}
-		}
-	});
-
-	Vue.component('message', {
-		props: ['title', 'body'],
-		data() {
-			return {
-				isVisible: true
-			};
-		},
-		template: `
-			<div class="message" v-show="isVisible">
-				<div>
-					{{ title }}
-					<button type="button" @click="hideModal">x</button>
-				</div>
-				<div>{{ body }}</div>
-			</div>
-		`,
-		methods: {
-			hideModal(){
-				this.isVisible = false;
-			}
-		}
-	});
-
-	const components = () => {
-		new Vue({
-			el: '#root4',
 		});
 	}
 
